d0b939d32b0e82622436347bccb72c5e9abeda6b
Vitor Mateus <vitor7jan@gmail.com>
2025-03-26 21:18:19 -0300

    converting moodle url to lower case

--- moodle-initialization.sh
+++ moodle-initialization.sh
@@ -6,6 +6,9 @@ if [ -z "$MOODLE_URL" ]; then
     exit 1
 fi
 
+# convert moodle url to lower case
+MOODLE_URL=${MOODLE_URL,,}
+
 # set instance id env var for nginx
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
 export INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
